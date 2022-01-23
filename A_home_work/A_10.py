fd432c9f0f020407591580170460b51468c1c6cd
nknsryo <nkns.ryo@gmail.com>
2022-01-23 21:46:44 +0900

    A_8

--- A_home_work/A_10.py
+++ A_home_work/A_10.py
@@ -1,5 +1,8 @@
-import random
+# import random
+#
+# dice = [1, 2, 3, 4, 5, 6]
+#
+# print(random.choice(dice))  # 1から6の整数をランダムに出力する
 
-dice = [1, 2, 3, 4, 5, 6]
 
-print(random.choice(dice))  # 1から6の整数をランダムに出力する
+def dice
---
+++ B_home_work/B_1_kuku_1.py
@@ -0,0 +1,5 @@
+
+for count_rows in range(1, 10):
+    for count_cullums in range(1, 10):
+        print(count_rows * count_cullums, end=' ')
+    print()
