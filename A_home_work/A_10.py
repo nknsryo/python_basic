55c612d4c677833729875a85d0681b17cc5fc9f1
nknsryo <nkns.ryo@gmail.com>
2022-01-24 08:56:25 +0900

    A_10

--- A_home_work/A_10.py
+++ A_home_work/A_10.py
@@ -3,6 +3,16 @@
 # dice = [1, 2, 3, 4, 5, 6]
 #
 # print(random.choice(dice))  # 1から6の整数をランダムに出力する
+#
 
 
-def dice
+import random
+
+
+def dice_random():
+    # dice_count = range(1, 6)
+    result = random.randint(1, 6)
+    return result
+
+
+print(dice_random())
