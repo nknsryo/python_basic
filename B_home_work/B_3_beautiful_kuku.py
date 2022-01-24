026b633a7abcdd0d29ea516524e4ef914d5270df
nknsryo <nkns.ryo@gmail.com>
2022-01-24 12:22:19 +0900

    B_3

--- B_home_work/B_3_beautiful_kuku.py
+++ B_home_work/B_3_beautiful_kuku.py
@@ -3,10 +3,10 @@ cullums = int(input("列数を入力してください: "))
 
 for count_rows in range(1, rows + 1):
     for count_cullums in range(1, cullums + 1):
-        counter_response = count_rows * count_cullums
-        if 1 <= counter_response <= 9:
-            counter_response = int(str(counter_response).rjust(0, 1))
-        else:
-            counter_response
-            print(f"{count_cullums} ×　{count_rows} = {counter_response} ｜", end=' ')
+        counter_response = str(count_rows * count_cullums)
+        # if 1 <= counter_response <= 9:
+        #     counter_response = int(str(counter_response).rjust(0, 1))
+        # else:
+        #     counter_response
+        print(f"{count_cullums} ×　{count_rows} = {counter_response.rjust(2)} ｜", end=' ')
     print()
