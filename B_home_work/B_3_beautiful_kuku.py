e0c693f1d8d13c1bda5d5058d28500b0e1d6a6ae
nknsryo <nkns.ryo@gmail.com>
2022-01-24 12:29:21 +0900

    B_3

--- B_home_work/B_3_beautiful_kuku.py
+++ B_home_work/B_3_beautiful_kuku.py
@@ -4,6 +4,7 @@ cullums = int(input("列数を入力してください: "))
 for count_rows in range(1, rows + 1):
     for count_cullums in range(1, cullums + 1):
         counter_response = str(count_rows * count_cullums)
+        # このやり方方向でうまくいかない
         # if 1 <= counter_response <= 9:
         #     counter_response = int(str(counter_response).rjust(0, 1))
         # else:
