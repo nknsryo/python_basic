aa45b12b921801fb7ed19732bd0331537dc871e7
nknsryo <nkns.ryo@gmail.com>
2022-01-25 22:01:20 +0900

    B_6

--- B_home_work/B_6.py
+++ B_home_work/B_6.py
@@ -8,16 +8,10 @@ import random
 
 # サイコロの面の数をユーザーに要求
 total_daice_side = int(input(f"サイコロの面の数は？: "))
-# 面に記載されている数をランダムに表示する
-daice_number = random.randint(1, total_daice_side)
 
 # サイコロを振る回数をユーザーに要求
 daice_action_times = int(input(f"何回振りますか?: "))
 
-# for daice in range(0, daice_action_times):
-#     for daice_number in range(0, total_daice_side):
-#         daice = list(str(random.randint(1, total_daice_side)))
-#     print(f"{daice}", end=' ')
 
 daice = []
 for result in range(0, daice_action_times):
