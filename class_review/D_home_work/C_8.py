2f3538e27ec3f231df006db986b9b4f2256b4485
nknsryo <nkns.ryo@gmail.com>
2022-01-27 22:02:13 +0900

    C_8

--- class_review/D_home_work/C_8.py
+++ class_review/D_home_work/C_8.py
@@ -22,15 +22,15 @@ class Customer:
             return 500
 
     def info_csv(self):
-        print(str(self.firstname) + " " + str(self.family_name) + " " + str(self.age) + " " + str(self.entry_fee()))
+        print(str(self.firstname) + " " + str(self.family_name) + "|" + str(self.age) + "|" + str(self.entry_fee()))
 
 
 # 単一顧客の情報取得をタブ区切りにも対応させてください
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.info_csv()  # Ken Tanaka  15  1000
+ken.info_csv()  # Ken Tanaka|15|1000
 
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
-tom.info_csv()  # Tom Ford    57  1500
+tom.info_csv()  # Tom Ford|57|1500
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu.info_csv()  # Ieyasu Tokugawa	73  1200
+ieyasu.info_csv()  # Ieyasu Tokugawa|73|1200
