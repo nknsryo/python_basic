13c85be9a02d7c2266619af732288c8716c8295c
nknsryo <nkns.ryo@gmail.com>
2022-01-27 20:53:17 +0900

    d_2

--- class_review/D_home_work/C_1.py
+++ class_review/D_home_work/C_1.py
@@ -1,11 +1,11 @@
-class customer:
+class Customer:
     def __init__(self, first_name, family_name):
         self.firstname = first_name
         self.familyname = family_name
 
-    def full_name():
-        print(f"{self.firstname}  {self.familyname}")
-      
+    def full_name(self):
+        print(f"{self.firstname} {self.familyname}")
+
 
 ken = Customer(first_name="Ken", family_name="Tanaka")
 ken.full_name()  # "Ken Tanaka" という値を返す
---
+++ class_review/D_home_work/C_2.py
@@ -0,0 +1,18 @@
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.age  # 15 という値を返す
+print(ken.age)
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+tom.age  # 57 という値を返す
+print(tom.age)
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.age  # 73 という値を返す
+print(ieyasu.age)
