696cf2eba6ffd6c188619a35af59462d8dc4317b
nknsryo <nkns.ryo@gmail.com>
2022-01-27 18:06:08 +0900

    d_1

--- class_review/D_home_work/customer.py
+++ class_review/D_home_work/customer.py
@@ -1,7 +1,11 @@
 class customer:
-    def __str__(self):
-        k
+    def __init__(self, first_name, family_name):
+        self.firstname = first_name
+        self.familyname = family_name
 
+    def full_name():
+        print(f"{self.firstname}  {self.familyname}")
+      
 
 ken = Customer(first_name="Ken", family_name="Tanaka")
 ken.full_name()  # "Ken Tanaka" という値を返す
